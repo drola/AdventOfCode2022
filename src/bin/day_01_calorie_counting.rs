0c0a00871fc3f3a776c32854ea984772fa800146
Matjaž Drolc <mdrolc@gmail.com>
2022-12-01 07:47:28 +0100

    Day 1: version with iterators

--- src/bin/day_01_calorie_counting.rs
+++ src/bin/day_01_calorie_counting.rs
@@ -1,5 +1,7 @@
 /// Solution to an Advent of Code problem, day 01, 2022
 /// https://adventofcode.com/2022/day/01
+use itertools::Itertools;
+use std::cmp::Reverse;
 use std::env;
 use std::fs;
 
@@ -7,32 +9,23 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
     let contents = fs::read_to_string(filename).expect("Cannot read file");
-    let lines = contents.lines();
+    let lines = contents.lines().map(|line| line.parse::<u64>());
+    let top_3_sums = lines
+        .group_by(|r| r.is_ok())
+        .into_iter()
+        .filter(|(k, _g)| *k)
+        .map(|(_k, g)| Reverse(g.map(|n| n.unwrap()).sum::<u64>()))
+        .k_smallest(3)
+        .map(|n| n.0) // Remove Reverse()
+        .collect::<Vec<_>>();
+    println!("Top 3 sums: {:?}", top_3_sums);
 
-    let mut max_cals: i64 = 0;
-    let mut current_cals: i64 = 0;
-    let mut sums: Vec<i64> = vec![];
-
-    for line in lines {
-        let cals: Result<i64, _> = line.parse();
-        if cals.is_ok() {
-            current_cals = current_cals + cals.unwrap();
-        } else {
-            sums.push(current_cals);
-            if current_cals > max_cals {
-                max_cals = current_cals;
-            }
-            current_cals = 0;
-        }
-    }
-    if current_cals > max_cals {
-        max_cals = current_cals;
-    }
-    sums.push(current_cals);
-    println!("Max cals: {}", max_cals);
-
-    sums.sort();
-    sums.reverse();
-
-    println!("Top 3 sum: {} + {} + {} = {}", sums[0], sums[1], sums[2], sums[0]+sums[1]+sums[2]);
+    println!(
+        "Elf with the most calories is carrying {} calories.",
+        top_3_sums[0]
+    );
+    println!(
+        "Top three elves are carrying a total of {} calories",
+        top_3_sums.iter().sum::<u64>()
+    );
 }
